cf5f52d91742cd87401faad4fe85cbf5e50dec97
pabloasturias <44870382+pabloasturias@users.noreply.github.com>
2025-12-17 19:31:08 +0100

    fix: Remove rogue SW trigger causing reload loop

--- index.tsx
+++ index.tsx
@@ -2,19 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { useRegisterSW } from 'virtual:pwa-register/react';
-
-const SWTrigger = () => {
-  useRegisterSW({
-    onNeedRefresh() {
-      if (confirm("New content available. Reload?")) {
-        window.location.reload();
-      }
-    },
-  });
-  return null;
-}
-
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
@@ -23,7 +10,6 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <SWTrigger />
     <App />
   </React.StrictMode>
 );
